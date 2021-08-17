b8e4d9c36409ee9a5ba3907fe1d26f6fc6e9e0bb
millene prado <millene_vprado@hotmail.com>
2021-08-17 15:08:46 -0300

    add mvc and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # list all tasks
+  # get 'tasks', to: 'tasks#index'
+
+  # # show a form to create a new task
+  # get 'tasks/new', to: 'tasks#new'
+
+  # # show a task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # save task into db
+  # post 'tasks', to: 'tasks#create'
+
+  # # show a form to update a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+
+  # # update task info into db
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
